8c2998953ae823b7e4ceba80b4158c14b1a9f3c0
Ghous Muhammad Shah Jeelani <codestargm@gmail.com>
2021-03-12 19:10:52 +0500

    First Commit ORBC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
+//!Images!\\
+import Linked from "./Img/linkedIn.png";
+
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import useStyles from "./CustomStyles";
 function App() {
+  const classes = useStyles();
+  const [bg, setBg] = useState("purple");
+
+  const randomColor = () => {
+    const random = "#" + Math.floor(Math.random() * 16777142).toString(16);
+    setBg(random);
+  };
+
+  useEffect(() => {
+    randomColor();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container style={{ background: bg }} maxWidth="xlg">
+        <Container spacing="center" align="center">
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "100vh" }}
+          >
+            <Grid item lg={3}>
+              <Paper className={classes.colored} elevation={0}>
+                <h1>{bg}</h1>
+              </Paper>
+              <p>Give A ⭐ And Get A Chance To Win My ❤️</p>
+            </Grid>
+          </Grid>
+        </Container>
+      </Container>
+    </>
   );
 }
 
---
+++ src/CustomStyles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  colored: {
+    borderRadius: "10px",
+    width: "164px",
+  },
+  coloredC: {
+    fontSize: "19px",
+    borderRadius: "50px",
+    width: "164px",
+  },
+  paper: {
+    background: "transparent",
+  },
+}));
